763e864690d463297edf2a8328886bfd6af5dddc
Hannes <hh@3yd.de>
2016-12-13 22:42:44 +0100

    make first version playable

---
+++ Assets/Scripts/Move_Objects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Objects : MonoBehaviour {
+    //update is called before the frame is 
+    private float mSpeed;
+    void Start()
+    {
+        mSpeed = 1f;
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(mSpeed*moveHorizontal * Time.deltaTime, 0.0f,mSpeed* moveVertical * Time.deltaTime);
+    }
+}
