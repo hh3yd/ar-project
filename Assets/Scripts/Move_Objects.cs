3e0b856e989ed116b026c2d551992dbca9631446
Hannes <hh@3yd.de>
2017-02-05 14:03:19 +0100

    new linee up

--- Assets/Scripts/Move_Objects.cs
+++ Assets/Scripts/Move_Objects.cs
@@ -1,18 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Move_Objects : MonoBehaviour {
     //update is called before the frame is 
-    private float mSpeed;
+    public float mSpeed;
+    private int count;
+    public Text countText;
     void Start()
-    {
-        mSpeed = 1f;
+    {   
+        count = 0;
+        SetCountText();
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+        //rb.AddForce(mv * mSpeed);
         transform.Translate(mSpeed*moveHorizontal * Time.deltaTime, 0.0f,mSpeed* moveVertical * Time.deltaTime);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tree"))
+        {/*
+            Renderer r = other.GetComponent<Renderer>();
+            Color col = r.material.color.linear;
+
+            col.r = r.material.color.r+1.0f;
+            r.material.SetColor("test",col);*/
+            //r.material.shader.
+            count++;
+            SetCountText();
+            if (count == 5)
+            {
+
+                other.gameObject.SetActive(false);
+                //show win panel
+            }
+            
+        }
+    }
+    private void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+
 }
